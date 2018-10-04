62e630ff02c8d517e441da6306c3d0754c4d07cb
Wisit Srimala <palm.future@gmail.com>
2018-10-04 20:34:40 +0700

    update middleware

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,3 @@
-const bodyParser = require('body-parser');
-
 const AuthController = require('./AuthController');
 const UserController = require('./UserController');
 const ColorController = require('./ColorController');
@@ -20,8 +18,6 @@ class Controller {
 
     initialController() {
         console.log('\x1b[31m', 'System:', '\x1b[37m', 'Initial Controller has been established successfully.');
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({ extended: false }));
 
         this.app.use('/api/users', UserController);
         this.app.use('/api/authentication', AuthController);
